4232dbb69c0a36016b8d9e8196f205e6c1981f79
Adam Hert <Adam Hert>
2016-04-24 20:53:10 -0700

    new fix

--- nmist/get_mnist_data_tf.py
+++ nmist/get_mnist_data_tf.py
@@ -85,7 +85,7 @@ def read_data_sets(train_dir, fake_data=False, one_hot=False):
     
     VALIDATION_SIZE = 25
     
-    train_images = nmist2[1:13][2:]
+    train_images = nmist2[:][:]
     print(train_images.shape)
     train_labels = nmist2[15:][2:]
    
