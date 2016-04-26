f7bf84cbee981d13f8192a92709842b0b060047a
Adam Hert <Adam Hert>
2016-04-25 19:27:26 -0700

    14

--- nmist/nmist.py
+++ nmist/nmist.py
@@ -66,8 +66,8 @@ h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 keep_prob = tf.placeholder("float")
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+W_fc2 = weight_variable([1024, 41])
+b_fc2 = bias_variable([41])
 
 y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
